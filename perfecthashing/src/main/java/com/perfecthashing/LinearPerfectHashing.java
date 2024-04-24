a472b7a6ecb8197e1a25cb62c4209066d471c9c2
nasr7322 <nasr7322@gmail.com>
2024-04-24 20:33:49 +0200

    yarabbb

--- perfecthashing/src/main/java/com/perfecthashing/LinearPerfectHashing.java
+++ perfecthashing/src/main/java/com/perfecthashing/LinearPerfectHashing.java
@@ -4,22 +4,26 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class LinearPerfectHashing<T> implements PerfectHashing<T> {
     private UniversalHashing firstLevelHashFunction;
-    private Map<Integer, ArrayList<T>> secondLevelTables;
+    private ArrayList<ArrayList<T>> secondLevelTables;
     private int size;
     private int rehashCount;
 
     public LinearPerfectHashing(int N) {
         firstLevelHashFunction = new UniversalHashing(32, 32);
-        secondLevelTables = new HashMap<>();
+        secondLevelTables = new ArrayList<>(N);
+        for (int i = 0; i < N; i++) {
+            secondLevelTables.add(new ArrayList<>());
+        }
         size = N;
         rehashCount = 0;
     }
 
     public int insert(T item) {
         int firstLevelHash = firstLevelHashFunction.hash(StringUtls.getStringKey(item.toString())) % size;
-        ArrayList<T> secondLevelTable = secondLevelTables.computeIfAbsent(firstLevelHash, k -> new ArrayList<>());
+        ArrayList<T> secondLevelTable = secondLevelTables.get(firstLevelHash);
         if (!secondLevelTable.contains(item)) {
             secondLevelTable.add(item);
             return 0;
@@ -30,7 +34,7 @@ public class LinearPerfectHashing<T> implements PerfectHashing<T> {
     public int delete(T item) {
         int firstLevelHash = firstLevelHashFunction.hash(StringUtls.getStringKey(item.toString())) % size;
         ArrayList<T> secondLevelTable = secondLevelTables.get(firstLevelHash);
-        if (secondLevelTable != null && secondLevelTable.remove(item)) {
+        if (secondLevelTable.remove(item)) {
             return 0;
         }
         return 1;
@@ -39,7 +43,7 @@ public class LinearPerfectHashing<T> implements PerfectHashing<T> {
     public boolean search(T item) {
         int firstLevelHash = firstLevelHashFunction.hash(StringUtls.getStringKey(item.toString())) % size;
         ArrayList<T> secondLevelTable = secondLevelTables.get(firstLevelHash);
-        return secondLevelTable != null && secondLevelTable.contains(item);
+        return secondLevelTable.contains(item);
     }
 
     public int getRehashCount() {
@@ -49,8 +53,8 @@ public class LinearPerfectHashing<T> implements PerfectHashing<T> {
     public Map<Integer, Integer> getTableSizes() {
         Map<Integer, Integer> tableSizes = new HashMap<>();
         tableSizes.put(0, size);  // size of the first-level table
-        for (Map.Entry<Integer, ArrayList<T>> entry : secondLevelTables.entrySet()) {
-            tableSizes.put(entry.getKey() + 1, entry.getValue().size());
+        for (int i = 0; i < size; i++) {
+            tableSizes.put(i + 1, secondLevelTables.get(i).size());
         }
         return tableSizes;
     }
